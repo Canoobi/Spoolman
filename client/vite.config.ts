677d2cfcce95b8ee8b821b059719074af484ced2
Carlo <carlo.gliech@gmx.de>
2025-12-18 01:56:28 +0100

    remove other languages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,22 +2,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
-const backendTarget = process.env.VITE_BACKEND_URL || "http://localhost:8000";
-
 export default defineConfig({
   base: "",
-  plugins: [react(), svgr()],
-  server: {
-    proxy: {
-      "/api": {
-        target: backendTarget,
-        changeOrigin: true,
-        ws: true,
-      },
-      "/config.js": {
-        target: backendTarget,
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react(), svgr()]
 });
