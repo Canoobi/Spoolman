a1e8da22b76348777d1b4ab7bb1a74c50a7f8ad0
Canoob <carlo.gliech@gmx.de>
2026-03-14 01:52:10 +0100

    fix: adjust Vite configuration base path and plugin setup

--- public-print-request/vite.config.ts
+++ public-print-request/vite.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: "/request/",
-    plugins: [react()],
     base: "",
+    plugins: [react()],
 });
